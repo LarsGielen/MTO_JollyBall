2ee463c14b8477818e4e9039b545dc5405da3065
larsgielen <larsgielen2003@gmail.com>
2025-04-29 15:19:02 +0200

    Blah

--- Embedded_code/Main/Main.ino
+++ Embedded_code/Main/Main.ino
@@ -1,22 +1,7 @@
 #include <Arduino.h>
 #include "MotorControl.h"
 #include "MPU6050_PID_YPR.h"
-#include "PID_v1.h"
-float ypr[3];
-float move_d[2];
-struct Vector3 {
-  float x, y, z;
 
-  void print(const char* label) {
-    Serial.print(label);
-    Serial.print(": (");
-    Serial.print(x, 3); Serial.print(", ");
-    Serial.print(y, 3); Serial.print(", ");
-    Serial.print(z, 3); Serial.println(")");
-  }
-};
-
-struct Vector3 resultvector;
 // --- Hardware and configuration parameters ---
 const int MOTOR_PUL_PINS[] = {3, 4, 5};
 const int MOTOR_DIR_PINS[] = {6, 7, 8};
@@ -30,101 +15,63 @@ const float STEP_SIZE = MOTOR_STEP_ANGLE / MICROSTEP;
 long motor_timers[] = {0, 0, 0};
 bool motor_flags[] = {false, false, false};
 
+// MPU 6050
+float ypr[3];
+float move_d[2];
+
 // PID config
-double pitchSetpoint = 0; // Upright
-double rollSetpoint = 0;  // Upright
-double pitchInput, rollInput;
+double pitchSetpoint = 0;
+double rollSetpoint = 0; 
+double max_angle = 90.0f * PI / 180.0f; // Angle in rad
 double pitchOutput, rollOutput;
 double Kp = 10000, Ki = 0, Kd = 0;
-double Kp_ = 10000, Ki_ = 0, Kd_ = 0;
 
-PID pitchPID(&pitchInput, &pitchOutput, &pitchSetpoint, Kp, Ki, Kd, 0, 0);
-
-
-PID rollPID(&rollInput, &rollOutput, &rollSetpoint, Kp_, Ki_, Kd_, 0, 0);
-
-int checksignage(float number)
-{
-  if(number >= 0){
-    return 1;
-  }
-  else return -1;
-}
+// Temp Functions
 
 
+// Main Functions
 void setup() {
-  // Initialize motor pins as outputs.
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_BLUE, HIGH);
+
   for (int i = 0; i < 3; i++) {
     pinMode(MOTOR_PUL_PINS[i], OUTPUT);
     pinMode(MOTOR_DIR_PINS[i], OUTPUT);
   }
-  setup_MPU6050();
-   pitchPID.SetOutputLimits(-1.0, 1.0); // or -90 to 90 for angles
-  rollPID.SetOutputLimits(-1.0, 1.0); // or -90 to 90 for angles
 
-   //turn the PID on
-  pitchPID.SetMode(AUTOMATIC);
-  rollPID.SetMode(AUTOMATIC);
+  setup_MPU6050();
+
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_BLUE, HIGH);
 }
 
 void loop() {
-  
-  //static float mv_dir[2];
-  // Try to get sensor data, but don't block. Returns from the function and proceeds on old data.
   bool b_newMpuData = readout_MPU6050(ypr);
- 
-  /*
-  // Z-Axis
-  mv_dir[0] = sin(fabs(ypr[2])) * -checksignage(ypr[2]);
-  // X-Axis
-  mv_dir[1] = sin(fabs(ypr[1])) * -checksignage(ypr[1]);
-*/
-/*
-  Serial.print("Yaw: ");
-  Serial.print(ypr[0]);
-  Serial.print(", Pitch: ");
-  Serial.print(ypr[1]);
-  Serial.print(", Roll: ");
-  Serial.println(ypr[2]);
-  Serial.print(", mvdir1: ");
-  Serial.print(mv_dir[1]);
-  Serial.print(", mvdir2: ");
-  Serial.println(mv_dir[0]);
-*/
-  pitchInput = (double)ypr[1];
-  rollInput = (double)ypr[2];
+  
+  // P only
+  double pitchError = pitchSetpoint - ypr[1];
+  double rollError = rollSetpoint - ypr[2];
 
-  //pitchPID.Compute();
-  //rollPID.Compute();
-  /*
-  Serial.print("PitchOut: "); Serial.print(pitchOutput);
-  Serial.print(", RollOut: "); Serial.println(rollOutput);
-  */
+  pitchOutput = map(-1.3f, max_angle, 1.3f, -1.0f, 1.0f);
+  rollOutput = map(-max_angle, max_angle, rollError, -1.0f, 1.0f);
+
   double resultspeed = (fabs(rollOutput) + fabs(pitchOutput))/2;
   double magnitude = sqrt((pitchOutput * pitchOutput )+ (rollOutput*rollOutput));
+  Serial.print("y: ");
+  Serial.print(pitchOutput);
+  Serial.print("p: ");
+  Serial.print(pitchOutput);
+  Serial.print("r: ");
+  Serial.print(pitchOutput);
 
-  Serial.print("resultspeed: ");
-  Serial.println(resultspeed);
-  /*
-  Serial.print("magnitude: ");
-  Serial.println(magnitude);
-  */
-  if( magnitude <= 0.01f)
-  {
-    //return;
-  }
-  //move_d[2] = { (float)(pitchOutput / magnitude), (float)(rollOutput / magnitude) };
-  
-  move_d[0] = 1;
-  move_d[1] = 0;
-  
-  move(move_d, 1.0f);
-  /*
-  Serial.print(", move_d0: ");
-  Serial.print(move_d[0]);
-  Serial.print(", move_d1: ");
-  Serial.print(move_d[1]);
-  Serial.print(", speedpercentage: ");
-  Serial.println(resultspeed);
-  */
+  Serial.print("pitchOutput: ");
+  Serial.print(pitchOutput);
+  Serial.print(" -- rollOutput: ");
+  Serial.println(rollOutput);
 }
